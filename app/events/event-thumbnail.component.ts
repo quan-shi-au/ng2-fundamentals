119230679c4d00142d8dbc63032ea58820b7c389
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2017-12-11 22:16:38 +1100

    no message

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -16,11 +16,17 @@ import { Component, Input } from '@angular/core'
             <span>&nbsp;</span>
             <span>{{event.location.city}}, {{event.location.country}}</span>
         </div>
+        <button class="btn btn-primary" (click)="handleClickMe()">
+            Click me! </button>
     </div>
 </div>
 
     `
 })
 export class EventThumbnailComponent {
-    @Input() event:any
+    @Input() event: any
+
+    handleClickMe() {
+        console.log('clicked!')
+    }
 }
